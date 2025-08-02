50b1bf8a4e8190787a3c9ac5a5fd6dbb65d8898a
Álefe Alves da Costa <alefealvesdacosta@gmail.com>
2025-08-02 14:37:13 -0400

    Adicionando implementação para verificação da existência de usuário no sistema.

--- minisudo/src/main.rs
+++ minisudo/src/main.rs
@@ -1,24 +1,36 @@
 extern crate bcrypt;
 use bcrypt::{DEFAULT_COST, hash, verify};
+use users::{get_current_uid, get_user_by_uid};
 
 fn main() {
     
     //let password = rpassword::read_password().unwrap();//Lê a senha do usuário pelo terminal
 
-    let password = rpassword::prompt_password("Digite sua senha: ").unwrap();
-    println!("Sua senha é: {}", password);
+    //Pega o id do usuário atual do sistema
+    let uid = get_current_uid();
     
-    let hashed = hash(password, DEFAULT_COST).unwrap();//Hash criado referente a senha
-    println!("Senha salva com sucesso!");
+    //Aqui deve ocorrer a verificação do usuário para caso ele esteja cadastrado no minisudo
+    if let Some(user) = get_user_by_uid(uid){
+        
+        //Definimos uma senha para o usuário, apenas teste.
+        let password = rpassword::prompt_password(format!("[minisudo] Digite uma senha para {}: ", user.name().to_string_lossy())).unwrap();
+        println!("Sua senha é: {}", password);
+        
+        //Senha passa pelo hash
+        let hashed = hash(password, DEFAULT_COST).unwrap();//Hash criado referente a senha
+        println!("Senha salva com sucesso!");
 
-    
-    let validation = rpassword::prompt_password("Informe sua senha para prosseguir: ").unwrap();
-    
-    let valid = verify(validation, &hashed).unwrap();//Verificação da senha do usuário
+        //Teste de validação de senha do usuário
+        let validation = rpassword::prompt_password(format!("[minisudo] senha para {}: ", user.name().to_string_lossy())).unwrap();
+        let valid = verify(validation, &hashed).unwrap();//Verificação da senha do usuário
+        
+        if valid {
+            println!("Sua senha foi validada com sucesso!");
+        }else {
+            println!("Erro ao tentar validar sua senha.",);
+        } 
 
-    if valid {
-        println!("Sua senha foi validada com sucesso!");
-    }else {
-        println!("Erro ao tentar validar sua senha.",);
-    }    
+    }else{
+        println!("Usuário não encontrado no sistema.");
+    }
 }
