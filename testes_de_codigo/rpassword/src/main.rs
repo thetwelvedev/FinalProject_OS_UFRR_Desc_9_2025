f82ca93bccb7439d3a4f04dc3705f21206cd8191
Leonardo Castro <student.leonardocastro@gmail.com>
2025-08-06 03:18:53 -0400

    Remoção

--- testes_de_codigo/rpassword/src/main.rs
+++
@@ -1,8 +0,0 @@
-use rpassword::read_password;
-
-fn main() {
-    println!("Digite sua senha:");
-    let senha = read_password().expect("Erro ao ler senha");
-    println!("Senha digitada (oculta): {}", senha);
-}
-
